d0f177344b522180867af70e1664226e2909ef75
djumanov <djumanovdev@gmail.com>
2025-11-08 10:27:06 +0500

    inline keyboard

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 from config import Config
-from callbacks import start, send_orders
+from callbacks import start, send_orders, send_about, change_language, send_settings
 
 
 def main() -> None:
@@ -19,6 +19,34 @@ def main() -> None:
         handler=MessageHandler(
             filters=Filters.text('📦 Buyurtmalarim'),
             callback=send_orders
+        )
+    )
+
+    dispatcher.add_handler(
+        handler=MessageHandler(
+            filters=Filters.text('ℹ️ Biz haqimizda'),
+            callback=send_about
+        )
+    )
+
+    dispatcher.add_handler(
+        handler=MessageHandler(
+            filters=Filters.text('⚙️ Sozlamalar'),
+            callback=send_settings
+        )
+    )
+
+    dispatcher.add_handler(
+        handler=MessageHandler(
+            filters=Filters.text('Orqaga'),
+            callback=start
+        )
+    )
+
+    dispatcher.add_handler(
+        handler=MessageHandler(
+            filters=Filters.text('Tilni o\'zgartirish'),
+            callback=change_language
         )
     )
 
--- callbacks.py
+++ callbacks.py
@@ -1,4 +1,11 @@
-from telegram import Update, ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
+from telegram import (
+    Update, 
+    ReplyKeyboardMarkup, 
+    KeyboardButton, 
+    WebAppInfo, 
+    InlineKeyboardMarkup, 
+    InlineKeyboardButton,
+)
 from telegram.ext import CallbackContext
 
 
@@ -36,4 +43,50 @@ def start(update: Update, context: CallbackContext) -> None:
     
 def send_orders(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Sizda hali birorta ham buyurtma yo`q')
-
+    
+def send_about(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('shu yerda joylashganmiz')
+    update.message.reply_markdown_v2('*Elektron pochta*: ||abror4work@gmail\.com||')
+    
+def send_settings(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(
+        text='⚙️ Sozlamalar',
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(
+                        text='🌐 Tilni o\'zgartirish'
+                    )
+                ],
+                [
+                    KeyboardButton(
+                        text='🌐 Raqamni o\'zgartirish'
+                    )
+                ],
+                [
+                    KeyboardButton(
+                        text='Orqaga'
+                    )
+                ],
+            ]
+        )
+    )
+    
+def change_language(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(
+        text='tilni tanlang',
+        reply_markup=InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    InlineKeyboardButton(
+                        text='Uzbek',
+                        callback_data='change_lang:uz'
+                    ),
+                    InlineKeyboardButton(
+                        text='English',
+                        callback_data='movie:34'
+                    )
+                ]
+            ]
+        )
+    )
